346e7c071ff3fda6874c22fa32cf00bad8d8eb05
Robert Zenz <Robert.Zenz@bonsaimind.org>
2016-07-17 18:30:28 +0200

    Added script for a new wine rpefix.

---
+++ wine/new-prefix.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env sh
+
+# Creates a new wine prefix in the current directory complete with
+# configuration and some helper scripts.
+#
+# Public Domain or CC0
+
+# Usage: newPrefix.sh
+# Just execute it in the directory in which you want the prefix.
+
+
+script_start="#!/usr/bin/env sh
+
+. \"\$(dirname \"\$0\")/base.sh\"
+"
+
+_create_script() {
+	echo "$2" > "$1"
+	chmod u+x "$1"
+}
+
+
+_create_script "base.sh" "#!/usr/bin/env sh
+
+export WINEPREFIX=\"$(pwd)\"
+"
+
+_create_script "cfg.sh" "$script_start
+winecfg
+"
+
+_create_script "explorer.sh" "$script_start
+./run.sh explorer.exe
+"
+
+_create_script "run.sh" "$script_start
+wine \$*
+"
+
+_create_script "tricks.sh" "$script_start
+winetricks \$*
+"
+
+./tricks.sh settings \
+	ddr=opengl \
+	fontsmooth=disable \
+	glsl=enabled \
+	multisampling=enabled \
+	remove_mono
+
+./tricks.sh settings \
+	sandbox
+
+./cfg.sh
+
